5555fd836635cfb3785ed1eddc193284af9185cc
Frederick PORT <developpeur@stud-ios.fr>
2020-12-12 19:27:02 +0100

    Initial Commit

---
+++ Shared/lesBasiquesApp.swift
@@ -0,0 +1,17 @@
+//
+//  lesBasiquesApp.swift
+//  Shared
+//
+//  Created by Frederick Port on 12/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct lesBasiquesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
